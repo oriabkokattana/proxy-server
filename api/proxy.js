d0190e4070e5add44df67c99f1761d887b1c66bc
oriabkokattana <riabkoleksandr@kattana.io>
2024-08-14 15:43:32 +0300

    fix: host url

--- api/proxy.js
+++ api/proxy.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = (req, res) => {
   const proxy = createProxyMiddleware({
-    target: 'http://broker-api.com:30002', // Replace with your actual API server
+    target: 'http://10.0.10.61:30002', // Replace with your actual API server
     changeOrigin: true,
     pathRewrite: {
       '^/api': '', // Remove '/api' prefix when forwarding
